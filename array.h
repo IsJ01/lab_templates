d8ab5da0dca72a6778b6c8fdf76dba1eddb3d51b
IsJ01 <islamishtalbagamaev27@gmail.com>
2026-05-06 22:22:15 +0300

    create optional and array

--- array.h
+++ array.h
@@ -7,49 +7,123 @@ class Array {
 public:
     // ======================== Constructors ========================
 
-    Array();                                // empty array, size 0
-    explicit Array(size_t size);            // default-constructed elements
-    Array(size_t size, const T& value);     // all elements = value
+    Array() {}       
+
+    explicit Array(size_t size): data_(new T[size]), size_(size) {} 
+
+    Array(size_t size, const T& value): data_(new T[size]), size_(size) {
+        std::fill(data_, data_ + size_, value);
+    }                              
 
     // =================== Copy and Move ===========================
 
-    Array(const Array& other);
-    Array(Array&& other);
-    Array& operator=(const Array& other);
-    Array& operator=(Array&& other);
+    Array(const Array& other): data_(new T[other.size_]), size_(other.size_) {
+        std::copy(other.data_, other.data_ + size_, data_);
+    }
+
+    Array(Array&& other): data_(other.data_), size_(other.size_) {
+        other.size_ = 0;
+        other.data_ = nullptr;
+    }
+
+    Array& operator=(const Array& other) {
+        if (this == &other) return *this;
+
+        Array temp(other);
+        swap(temp);
+        return *this;
+    }
+
+    Array& operator=(Array&& other) {
+        if (this == &other) return *this;
+
+        delete[] this->data_;
+
+        this->size_ = other.size_;
+        this->data_ = other.data_;
+
+        other.size_ = 0;
+        other.data_ = nullptr;
+
+        return *this;
+    }
 
     // =================== Destructor ==============================
 
-    ~Array();
+    ~Array() {
+        delete[] data_;
+    }
 
     // =================== Element access ==========================
 
-    T&       operator[](size_t index);
-    const T& operator[](size_t index) const;
+    T&       operator[](size_t index) {
+        return data_[index];
+    }
 
-    T&       front();
-    const T& front() const;
+    const T& operator[](size_t index) const {
+        return data_[index];
+    }
 
-    T&       back();
-    const T& back() const;
+    T&       front() {
+        return data_[0];
+    }
 
-    T*       data();
-    const T* data() const;
+    const T& front() const {
+        return data_[0];
+    }
+
+    T&       back() {
+        return data_[size_ - 1];
+    }
+
+    const T& back() const {
+        return data_[size_ - 1];
+    }
+
+    T*       data() {
+        return data_;
+    }
+    
+    const T* data() const {
+        return data_;
+    }
 
     // =================== Capacity ================================
 
-    size_t size() const;
-    bool   empty() const;
+    size_t size() const  {
+        return size_;
+    }
+
+    bool   empty() const {
+        return size_ == 0;
+    }
 
     // =================== Operations ==============================
 
-    void fill(const T& value);
-    void swap(Array& other);
+    void fill(const T& value) {
+        std::fill(data_, data_ + size_, value);
+    }
+
+    void swap(Array& other) {
+        std::swap(size_, other.size_);
+        std::swap(data_, other.data_);
+    }
 
     // =================== Comparison ==============================
 
-    bool operator==(const Array& rhs) const;
-    bool operator!=(const Array& rhs) const;
+    bool operator==(const Array& rhs) const {
+        if (this->size_ != rhs.size_) return false;
+
+        for (size_t i = 0; i < this->size_; i++) {
+            if ((*this)[i] != rhs[i]) return false;
+        }
+
+        return true;
+    }
+
+    bool operator!=(const Array& rhs) const {
+        return !(*this == rhs);
+    }
 
 private:
     T*     data_ = nullptr;
--- optional.h
+++ optional.h
@@ -3,59 +3,205 @@
 #include <new>
 #include <type_traits>
 #include <utility>
+#include <algorithm>
 
 template <typename T>
 class Optional {
 public:
     // ======================== Constructors ========================
 
-    Optional();
-    Optional(const T& value);
-    Optional(T&& value);
+    Optional() = default;
+
+    Optional(const T& value) {
+        new (ptr()) T(value);
+        has_value_ = true;
+    }
+
+    Optional(T&& value) {
+        new (ptr()) T(value);
+        has_value_ = true;
+    }
 
     // =================== Copy and Move ===========================
 
-    Optional(const Optional& other);
-    Optional(Optional&& other);
-    Optional& operator=(const Optional& other);
-    Optional& operator=(Optional&& other);
-    Optional& operator=(const T& value);
-    Optional& operator=(T&& value);
+    Optional(const Optional& other) {
+        if (other.has_value_) {
+            new (ptr()) T(*other.ptr());
+            has_value_ = true; 
+        }
+    }
+
+    Optional(Optional&& other) {
+        if (other.has_value_) {
+            new (ptr()) T{std::move(*other.ptr())};
+            has_value_ = true;
+            other.ptr()->~T();
+            other.has_value_ = false;
+        }
+    }
+
+    Optional& operator=(const Optional& other) {
+        if (this == &other) return *this;
+        
+        if (other.has_value_) {
+            if (has_value_) {
+                *ptr() = *other.ptr();
+            } else {
+                new (ptr()) T(*other.ptr());
+                has_value_ = true; 
+            }
+            
+        } else {
+            reset();
+        }
+
+        return *this;
+    }
+
+    Optional& operator=(Optional&& other) {
+        if (this == &other) return *this;
+        
+        if (other.has_value_) {
+            if (has_value_) {
+                *ptr() = std::move(*other.ptr());
+            } else {
+                new (ptr()) T{std::move(*other.ptr())};
+                has_value_ = true;
+            }
+            other.ptr()->~T();
+            other.has_value_ = false;
+        } else {
+            reset();
+        }
+
+        return *this;
+    }
+
+    Optional& operator=(const T& value) {
+        if (has_value_) {
+            *ptr() = value;
+        } else {
+            new (ptr()) T(value);
+            has_value_ = true; 
+        }
+        
+        return *this;
+    }
+
+    Optional& operator=(T&& value) {
+        if (has_value_) {
+            *ptr() = std::move(value);
+        } else {
+            new (ptr()) T{std::move(value)};
+            has_value_ = true; 
+        }
+        
+        return *this;
+    }
 
     // =================== Destructor ==============================
 
-    ~Optional();
+    ~Optional() {
+        reset();
+    }
 
     // =================== Observers ===============================
 
-    bool has_value() const;
-    explicit operator bool() const;
+    bool has_value() const {
+        return has_value_;
+    }
 
-    T&       value();
-    const T& value() const;
+    explicit operator bool() const {
+        return has_value_;
+    }
 
-    T        value_or(const T& default_value) const;
+    T&       value() {
+        return *ptr();
+    }
+    
+    const T& value() const {
+        return *ptr();
+    }
 
-    T&       operator*();
-    const T& operator*() const;
+    T        value_or(const T& default_value) const {
+        return has_value_ ? *ptr() : default_value;
+    }
 
-    T*       operator->();
-    const T* operator->() const;
+    T&       operator*() {
+        return *ptr();
+    }
+
+    const T& operator*() const {
+        return *ptr();
+    }
+
+    T*       operator->() {
+        return ptr();
+    }
+
+    const T* operator->() const {
+        return ptr();
+    }
 
     // =================== Modifiers ===============================
 
     template <typename... Args>
-    T& emplace(Args&&... args);
+    T& emplace(Args&&... args) {
+        reset();
 
-    void reset();
-    void swap(Optional& other);
+        new (ptr()) T(std::forward<Args>(args)...);
+
+        has_value_ = true;
+        return *ptr();
+    }
+
+    void reset() {
+        if (has_value_) {
+            ptr()->~T();
+            has_value_ = false;
+        }
+    }
+
+    void swap(Optional& other) {
+        if (has_value_ && other.has_value_) {
+            std::swap(*ptr(), *other.ptr());
+        }
+        else if (has_value_) {
+            other.has_value_ = true;
+            new (other.ptr()) T(std::move(*ptr()));
+            ptr()->~T();
+            has_value_ = false;
+        }
+        else if (other.has_value_) {
+            has_value_ = true;
+            new (ptr()) T(std::move(*(other.ptr())));
+            other.ptr()->~T();
+            other.has_value_ = false;
+        }
+    }
 
     // =================== Comparison ==============================
 
-    bool operator==(const Optional& rhs) const;
-    bool operator!=(const Optional& rhs) const;
+    bool operator==(const Optional& rhs) const {
+        if (this->has_value_ != rhs.has_value_) return false;
+        if (*ptr() != *(rhs.ptr())) return false;
+        return true;
+    }
+
+    bool operator!=(const Optional& rhs) const {
+        return !(*this == rhs);
+    }
 
 private:
+
+    T *ptr() {
+        return reinterpret_cast<T*>(&storage_);
+    }
+
+    const T *ptr() const {
+        return reinterpret_cast<const T*>(&storage_);
+    }
+
     alignas(T) unsigned char storage_[sizeof(T)];
     bool has_value_ = false;
 };
